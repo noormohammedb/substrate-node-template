f76474b6ce9a99be608d0a5ceca33ea16d6eb1d5
Noor Mohammed <noormohammedb@protonmail.com>
2022-12-30 15:01:08 +0400

    Basic substrate kittie
    custom types - kitty struct, Gender
    storage items - kitti map, kitty count, kitti owned map
    events, error, generate dna function

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,36 +4,124 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::{ValueQuery, *},
+		BoundedVec, Twox64Concat,
+	};
 	use frame_system::pallet_prelude::*;
+
+	use frame_support::traits::{Currency, Randomness};
 
 	// The struct on which we build all of our Pallet logic.
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
-	/* Placeholder for defining custom types. */
+	// Allows easy access our Pallet's `Balance` type. Comes from `Currency` interface.
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
-	/* Placeholder for defining custom storage items. */
+	// The Gender type used in the `Kitty` struct
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub enum Gender {
+		Male,
+		Female,
+	}
+
+	// Struct for holding kitty information
+	#[derive(Clone, Copy, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Kitty<T: Config> {
+		// Using 16 bytes to represent a kitty DNA
+		pub dna: [u8; 16],
+		// `None` assumes not for sale
+		pub price: Option<BalanceOf<T>>,
+		pub gender: Gender,
+		pub owner: T::AccountId,
+	}
+
+	/// Keeps track of the number of kitties in existence.
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	/// Maps the kitty struct to the kitty DNA.
+	#[pallet::storage]
+	pub(super) type Kitties<T: Config> = StorageMap<_, Twox64Concat, [u8; 16], Kitty<T>>;
+
+	/// Track the kitties owned by each account.
+	#[pallet::storage]
+	pub(super) type KittiesOwned<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<[u8; 16], T::MaxKittiesOwned>,
+		ValueQuery,
+	>;
 
 	// Your Pallet's configuration trait, representing custom external types and interfaces.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		/// The Currency handler for the kitties pallet.
+		type Currency: Currency<Self::AccountId>;
+
+		/// The maximum amount of kitties a single account can own.
+		#[pallet::constant]
+		type MaxKittiesOwned: Get<u32>;
+
+		/// The type of Randomness we want to specify for this pallet.
+		type KittyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
 	// Your Pallet's events.
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
+	pub enum Event<T: Config> {
+		/// A new kitty was successfully created.
+		Created { kitti: [u8; 16], owner: T::AccountId },
+	}
 
 	// Your Pallet's error messages.
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		/// An account may only own `MaxKittiesOwned` kitties.
+		TooManyOwned,
+		/// This kitty already exists!
+		DuplicateKitty,
+		/// An overflow has occurred!
+		Overflow,
+	}
 
 	// Your Pallet's callable functions.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {}
 
 	// Your Pallet's internal functions.
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		// Generates and returns a random kitty DNA.
+		fn gen_dna() -> ([u8; 16], Gender) {
+			// Create randomness
+			let random = T::KittyRandomness::random(&b"dna"[..]).0;
+
+			// Create randomness payload. Multiple kitties can be generated in the same block,
+			// retaining uniqueness.
+			let unique_payload = (
+				random,
+				frame_system::Pallet::<T>::extrinsic_index().unwrap_or_default(),
+				frame_system::Pallet::<T>::block_number(),
+			);
+
+			// Turns into a byte array
+			let encoded_payload = unique_payload.encode();
+			let hash = frame_support::Hashable::blake2_128(&encoded_payload);
+
+			// Generate Gender
+			if hash[0] % 2 == 0 {
+				(hash, Gender::Male)
+			} else {
+				(hash, Gender::Female)
+			}
+		}
+	}
 }
