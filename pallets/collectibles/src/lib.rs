1047f00640a1aaec36f6fbc0b4c8500e8499794b
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-08 00:08:38 +0400

    collectibles basic pallet

---
+++ pallets/collectibles/src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(PhantomData<T>);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		//
+	}
+}
