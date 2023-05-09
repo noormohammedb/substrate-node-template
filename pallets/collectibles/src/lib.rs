c703eb2e784203bfa7fc16e00daa3fd99a2209b4
Noor Mohammed <noormohammedb@protonmail.com>
2023-05-09 21:50:31 +0400

    Collectibles datastructures and types

--- pallets/collectibles/src/lib.rs
+++ pallets/collectibles/src/lib.rs
@@ -4,15 +4,58 @@ pub use pallet::*;
 
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::{StorageMap, *},
+		Twox64Concat,
+	};
 	use frame_system::pallet_prelude::*;
 
+	use frame_support::traits::{Currency, Randomness};
+
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
+	// #[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(PhantomData<T>);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		//
+	pub trait Config: frame_system::Config + scale_info::TypeInfo {
+		type Currency: Currency<Self::AccountId>;
+		type CollectionRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+		#[pallet::constant]
+		type MaximumOwned: Get<u32>;
 	}
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub enum Color {
+		Red,
+		Yellow,
+		Blue,
+		Green,
+	}
+
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub struct Collectible<T: Config> {
+		pub unique_id: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub color: Color,
+		pub owner: T::AccountId,
+	}
+
+	#[pallet::storage]
+	pub(super) type CollectiblesCount<T: Config> = StorageValue<_, u64, ValueQuery>;
+	#[pallet::storage]
+	pub(super) type CollectibleMap<T: Config> =
+		StorageMap<_, Twox64Concat, [u8; 16], Collectible<T>>;
+	#[pallet::storage]
+	// #[scale_info(skip_type_params(T))]
+	pub(super) type OwnerOfCollectibles<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<[u8; 16], T::MaximumOwned>,
+		ValueQuery,
+	>;
 }
